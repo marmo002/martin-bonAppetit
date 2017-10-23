e8ec15abcea33b0184f45fd836c1aa79ac01b663
marmo002 <marmo_02@hotmail.com>
2017-10-23 18:21:56 -0400

    Create actions for create a session and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    #code
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate( params[:password] )
+      session[:user_id] = user.id
+      redirect_to restaurants_url, notice: "Logged in"
+    else
+      render :new, notice: "Wrong email/password combination"
+    end
   end
 
   def destroy
-    #code
+    session[:user_id] = nill
+    redirect_to restaurants_url, notice: "Logged out
   end
 
 
