7853aa4301c7bae9cf43eb3814d1e9cf0a6091d1
Marian Serna <marianhalliday@gmail.com>
2017-10-23 16:02:55 -0400

    Adding status column to reservations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023191853) do
+ActiveRecord::Schema.define(version: 20171023200204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20171023191853) do
     t.integer "num_seats", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "pending", null: false
   end
 
   create_table "restaurants", force: :cascade do |t|
