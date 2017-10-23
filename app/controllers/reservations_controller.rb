bdeb17b0f4d8d0f0cc79cf28c25066354df10108
Marian Serna <marianhalliday@gmail.com>
2017-10-23 17:36:40 -0400

    Adding nested routes for reservations + reservations controller and reservations#new & reservations#create actions

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,16 +1,32 @@
 class ReservationsController < ApplicationController
 
+  before_action :require_login
+  before_action :load_restaurant
+
   def new
-    @reservation = Reservation.new
+    @reservation = @restaurant.reservations.new
   end
 
   def create
+    @reservation = @restaurant.reservations.new(reservation_params)
+    @reservation.user_id = current_user.id
 
+    if @reservation.save
+      flash[:notice] = "Thanks for choosing us. Please wait for a confirmation of your reservation"
+      redirect_to restaurant_url(@restaurant)
+    else
+      flash[:alert] = "Sorry. Your reservation couldn't be processed."
+      render :new_restaurant_reservation_url
+    end
   end
 
   private
 
   def reservation_params
-    # comment
+    params.require(:reservation).permit(:date, :time, :num_seats)
+  end
+
+  def load_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
