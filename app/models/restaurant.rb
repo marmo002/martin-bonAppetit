66aaad679b846e18bb67d053d224ed6bc3aa7b80
Marian Serna <marianhalliday@gmail.com>
2017-10-23 15:27:19 -0400

    Adding validations to models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reservations
   has_many :users, through: :reservations
+
+  validates :name, :address, :city, :price_range, :total_seats, :open_hour, :close_hour, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   has_many :reservations
   has_one :restaurant
-  
+
+  validates :name, :email, :phone, presence: true
+
+
+  def is_admin?
+    self.restaurant.present?
+  end
 end
