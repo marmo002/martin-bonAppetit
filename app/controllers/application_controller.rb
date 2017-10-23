a3935effa9f7b842f48120efb5cb49e96abd4d77
Marian Serna <marianhalliday@gmail.com>
2017-10-23 17:02:36 -0400

    Creating require_login method in application controller + reservations#new controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if
+      session[:user_id]
+  end
+
+  helper_method :current_user
+
+  def require_login
+    if !current_user
+      redirect_to new_session_path
+    end
+  end
 end
---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,10 @@
+class ReservationsController < ApplicationController
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+
+  end
+end
