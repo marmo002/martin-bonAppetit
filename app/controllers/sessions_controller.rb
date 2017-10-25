1ed59dba82efe81582583854180c5ebc4a2712ba
marmo002 <marmo_02@hotmail.com>
2017-10-25 11:55:38 -0400

    Worked on Sessions#create method. Add custom validation to login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,35 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    
-    if user && user.authenticate( params[:password] )
-      session[:user_id] = user.id
-      redirect_to restaurants_url
+
+    if params[:email].present?
+      if valid_email?(params[:email])
+        if params[:password].present?
+          user = User.find_by(email: params[:email])
+          if user && user.authenticate( params[:password] )
+            session[:user_id] = user.id
+            redirect_to restaurants_url
+          else
+            flash[:notice] = "Wrong email/password combination"
+            render :new
+            flash.clear
+          end
+        else
+          flash[:notice] = "Password field can't be blank"
+          render :new
+          flash.clear
+        end
+      else
+        flash[:notice] = "Try to type a email please"
+        render :new
+        flash.clear
+      end
     else
-      render :new, notice: "Wrong email/password combination"
+      flash[:notice] = "Email field can't be blank"
+      render :new
+      flash.clear
     end
+
   end
 
   def destroy
@@ -19,4 +40,13 @@ class SessionsController < ApplicationController
     redirect_to restaurants_url
   end
 
+  private
+  def valid_email?(email)
+    rex = /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+    if email.match(rex)
+      return  true
+    else
+      return false
+    end
+  end
 end
